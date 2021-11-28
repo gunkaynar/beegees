b7b74816b4ef3d1b87655aab28cd64a024f09316
gunkaynar <gunkaynar@gmail.com>
2021-11-28 14:31:48 +0300

    model adjusted

--- src/model.py
+++ src/model.py
@@ -5,7 +5,6 @@ from dnn import *
 from LoadImage import load_data
 
 
-
 train_x_orig, train_y, test_x_orig, test_y = load_data()
 
 m_train = train_x_orig.shape[0]
@@ -21,102 +20,13 @@ print ("test_x_orig shape: " + str(test_x_orig.shape))
 print ("test_y shape: " + str(test_y.shape))
 
 
-
-train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0], -1).T  
-test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
-
-train_x = train_x_flatten/255.
-test_x = test_x_flatten/255.
-
-print ("train_x's shape: " + str(train_x.shape))
-print ("test_x's shape: " + str(test_x.shape))
-
-n_x = 12288 #arrayimizin size'ı     
-n_h = 7 #layer sayısı
-n_y = 1 #output sayısı
-layers_dims = (n_x, n_h, n_y)
-learning_rate = 0.0075 #adjust this
-
-
-def two_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 3000, print_cost=False):
-
-    
-    np.random.seed(1)
-    grads = {}
-    costs = []                              
-    (n_x, n_h, n_y) = layers_dims
-    
-
-    parameters = initialize_parameters(n_x, n_h, n_y)
-    
-    
-    W1 = parameters["W1"]
-    b1 = parameters["b1"]
-    W2 = parameters["W2"]
-    b2 = parameters["b2"]
-    
-
-    for i in range(0, num_iterations):
-
-
-        A1, cache1 = linear_activation_forward(X, W1, b1, "relu")
-        A2, cache2 = linear_activation_forward(A1, W2, b2, "sigmoid")
-        
-
-        cost = compute_cost(A2, Y)
-        
-
-        dA2 = - (np.divide(Y, A2) - np.divide(1 - Y, 1 - A2))
-        
-
-        
-        dA1, dW2, db2 = linear_activation_backward(dA2, cache2, "sigmoid")
-        dA0, dW1, db1 = linear_activation_backward(dA1, cache1, "relu")
-        
-        grads['dW1'] = dW1
-        grads['db1'] = db1
-        grads['dW2'] = dW2
-        grads['db2'] = db2
-        
-
-        parameters = update_parameters(parameters, grads, learning_rate)
-        
-
-        W1 = parameters["W1"]
-        b1 = parameters["b1"]
-        W2 = parameters["W2"]
-        b2 = parameters["b2"]
-        
-        if print_cost and i % 100 == 0 or i == num_iterations - 1:
-            print("Cost after iteration {}: {}".format(i, np.squeeze(cost)))
-        if i % 100 == 0 or i == num_iterations:
-            costs.append(cost)
-
-    return parameters, costs
-
+  
 def plot_costs(costs, learning_rate=0.0075):
     plt.plot(np.squeeze(costs))
     plt.ylabel('cost')
     plt.xlabel('iterations (per hundreds)')
     plt.title("Learning rate =" + str(learning_rate))
     plt.show()
-
-
-
-
-parameters, costs = two_layer_model(train_x, train_y, layers_dims = (n_x, n_h, n_y), num_iterations = 2, print_cost=False)
-
-print("Cost after first iteration: " + str(costs[0]))
-
-
-
-
-parameters, costs = two_layer_model(train_x, train_y, layers_dims = (n_x, n_h, n_y), num_iterations = 2500, print_cost=True)
-plot_costs(costs, learning_rate)
-
-predictions_train = predict(train_x, train_y, parameters)
-predictions_test = predict(test_x, test_y, parameters)
-
 
 
 
@@ -157,14 +67,11 @@ def L_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 30
 
 
 
-parameters, costs = L_layer_model(train_x, train_y, layers_dims, num_iterations = 1, print_cost = False)
 
 print("Cost after first iteration: " + str(costs[0]))
 
 
-
-
 parameters, costs = L_layer_model(train_x, train_y, layers_dims, num_iterations = 2500, print_cost = True)
 
-
+plot_costs(costs, learning_rate)
 
