e25a4f7273c955e5a95e4aab77dd23483b203949
gunkaynar <45332095+gunkaynar@users.noreply.github.com>
2021-12-26 17:01:14 +0300

    Update train.py

--- Transfer Learning Model/train.py
+++ Transfer Learning Model/train.py
@@ -10,24 +10,30 @@ data_dir = '/mnt/gunkaynar/project/beegees/data/train'
 
 def load_split_train_test(datadir, valid_size = .2):
     train_transforms = transforms.Compose([transforms.Resize((224,224)),transforms.ToTensor(),])    
+    validation_transforms = transforms.Compose([transforms.Resize((224,224)),transforms.ToTensor(),])    
     test_transforms = transforms.Compose([transforms.Resize((224,224)),transforms.ToTensor(),])    
     train_data = datasets.ImageFolder(datadir,transform=train_transforms)
+    validation_data = datasets.ImageFolder(datadir,transform=validation_transforms)    
     test_data = datasets.ImageFolder(datadir,transform=test_transforms)    
     num_train = len(train_data)
     indices = list(range(num_train))
     split = int(np.floor(valid_size * num_train))
     np.random.shuffle(indices)
     from torch.utils.data.sampler import SubsetRandomSampler
-    train_idx, test_idx = indices[split:], indices[:split]
+    train_idx, validation_idx,test_idx = indices[split:], indices[:round(split/2)], indices[round(split/2):split]
     train_sampler = SubsetRandomSampler(train_idx)
+    validation_sampler = SubsetRandomSampler(validation_idx)
     test_sampler = SubsetRandomSampler(test_idx)
     trainloader = torch.utils.data.DataLoader(train_data, sampler=train_sampler, batch_size=64)
+    validationloader = torch.utils.data.DataLoader(test_data, sampler=test_sampler, batch_size=64)
     testloader = torch.utils.data.DataLoader(test_data, sampler=test_sampler, batch_size=64)
-    return trainloader, testloader
-trainloader, testloader = load_split_train_test(data_dir, .2)
+    return trainloader, validationloader ,testloader
+trainloader, validationloader,testloader = load_split_train_test(data_dir, .2)
 
 print(trainloader.dataset.classes)
+print(validationloader.dataset.classes)
 print(testloader.dataset.classes)
+
 gpu_id = int(input("GPU index: "))
 if torch.cuda.is_available():
     device = torch.device(f"cuda:{gpu_id}")
@@ -39,15 +45,10 @@ print("We're using =>", device)
 model = models.resnet50(pretrained=True)
 print(model)
 
-
 for param in model.parameters():
     param.requires_grad = False
     
-model.fc = nn.Sequential(nn.Linear(2048, 512),
-                                 nn.ReLU(),
-                                 nn.Dropout(0.2),
-                                 nn.Linear(512, 10),
-                                 nn.LogSoftmax(dim=1))
+model.fc = nn.Sequential(nn.Linear(2048, 512),nn.ReLU(),nn.Dropout(0.2),nn.Linear(512, 10),nn.LogSoftmax(dim=1))
 criterion = nn.NLLLoss()
 optimizer = optim.Adam(model.fc.parameters(), lr=0.003)
 model.to(device)
@@ -56,7 +57,7 @@ epochs = 1
 steps = 0
 running_loss = 0
 print_every = 10
-train_losses, test_losses = [], []
+train_losses, validation_losses = [], []
 for epoch in range(epochs):
     for inputs, labels in trainloader:
         steps += 1
@@ -69,33 +70,32 @@ for epoch in range(epochs):
         running_loss += loss.item()
         
         if steps % print_every == 0:
-            test_loss = 0
+            validation_loss = 0
             accuracy = 0
             model.eval()
             with torch.no_grad():
-                for inputs, labels in testloader:
+                for inputs, labels in validationloader:
                     inputs, labels = inputs.to(device),labels.to(device)
                     logps = model.forward(inputs)
                     batch_loss = criterion(logps, labels)
-                    test_loss += batch_loss.item()
-                    
+                    validation_loss += batch_loss.item()
                     ps = torch.exp(logps)
                     top_p, top_class = ps.topk(1, dim=1)
                     equals = top_class == labels.view(*top_class.shape)
                     accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
             train_losses.append(running_loss/len(trainloader))
-            test_losses.append(test_loss/len(testloader))                    
+            validation_losses.append(validation_loss/len(validationloader))                    
             print(f"Epoch {epoch+1}/{epochs}.. "
                   f"Train loss: {running_loss/print_every:.3f}.. "
-                  f"Test loss: {test_loss/len(testloader):.3f}.. "
-                  f"Test accuracy: {accuracy/len(testloader):.3f}")
+                  f"Validation loss: {validation_loss/len(validationloader):.3f}.. "
+                  f"Validation accuracy: {accuracy/len(validationloader):.3f}")
             running_loss = 0
             model.train()
 torch.save(model, 'beemodel.pth')
 
 
 plt.plot(train_losses, label='Training loss')
-plt.plot(test_losses, label='Validation loss')
+plt.plot(validation_losses, label='Validation loss')
 plt.legend(frameon=False)
 plt.savefig("loss.png")
 
